3c6da9e3835244842a3fb430701cfc477e3836f8
John Sanda <john.sanda@gmail.com>
2020-01-18 23:55:02 -0500

    add test util function to wait for Reaper instance to be deployed

--- test/e2eutil/wait_util.go
+++ test/e2eutil/wait_util.go
@@ -1,11 +1,18 @@
 package e2eutil
 
 import (
+	goctx "context"
+	v1alpha1 "github.com/jsanda/reaper-operator/pkg/apis/reaper/v1alpha1"
 	"github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
 	framework "github.com/operator-framework/operator-sdk/pkg/test"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apimachinery/pkg/types"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"testing"
 	"time"
 )
+
+type ReaperConditionFunc func(reaper *v1alpha1.Reaper) (bool, error)
 
 func WaitForOperatorDeployment(t *testing.T,
 	f *framework.Framework,
@@ -15,4 +22,30 @@ func WaitForOperatorDeployment(t *testing.T,
 	timeout time.Duration,) error {
 
 	return e2eutil.WaitForDeployment(t, f.KubeClient, namespace, name, 1, retryInterval, timeout)
+}
+
+func WaitForReaper(t *testing.T,
+	f *framework.Framework,
+	namespace string,
+	name string,
+	retryInterval time.Duration,
+	timeout time.Duration) error {
+
+	return wait.Poll(retryInterval, timeout, func() (bool, error) {
+		reaper := &v1alpha1.Reaper{}
+
+		if err := f.Client.Get(goctx.TODO(), types.NamespacedName{Namespace: namespace, Name: name}, reaper); err!= nil {
+			if apierrors.IsNotFound(err) {
+				t.Logf("Reaper %s not found\n", name)
+				return false, nil
+			}
+			return false, nil
+		}
+		if reaper.Status.ReadyReplicas == 0 {
+			t.Logf("Waiting for Reaper %s\n", name)
+			return false, nil
+		}
+
+		return true, nil
+	})
 }
