2ef2045df4b3a0055aebeba78620250011802e37
John Sanda <john.sanda@gmail.com>
2020-02-02 15:56:27 -0500

    introduce service interface and validate that CassandraBackend.ContactPoints is set

--- pkg/backend/backend.go
+++ pkg/backend/backend.go
@@ -9,11 +9,24 @@ type CassandraValidationError error
 
 var (
 	ClusterNameRequired CassandraValidationError = errors.New("CassandraBackend.ClusterName is required")
+	ContactPointsRequired CassandraValidationError = errors.New("CassandraBackend.ContactPoints is required")
 )
 
+type BackendService interface {
+	Validate(cfg v1alpha1.ServerConfig) error
 
+	CheckDefaults(cfg *v1alpha1.ServerConfig) bool
 
-func Validate(cfg v1alpha1.ServerConfig) error {
+	InitBackend(r *v1alpha1.Reaper)
+}
+
+type defaultBackendService struct{}
+
+func NewBackendService() BackendService {
+	return &defaultBackendService{}
+}
+
+func (b *defaultBackendService) Validate(cfg v1alpha1.ServerConfig) error {
 	if cfg.StorageType == "" || cfg.StorageType == v1alpha1.Memory {
 		return nil
 	}
@@ -22,7 +35,19 @@ func Validate(cfg v1alpha1.ServerConfig) error {
 		if cfg.CassandraBackend == nil || cfg.CassandraBackend.ClusterName == "" {
 			return ClusterNameRequired
 		}
+
+		if len(cfg.CassandraBackend.ContactPoints) == 0 {
+			return ContactPointsRequired
+		}
 	}
 
 	return nil
+}
+
+func (b *defaultBackendService) CheckDefaults(cfg *v1alpha1.ServerConfig) bool {
+	return false
+}
+
+func (b *defaultBackendService) InitBackend(r *v1alpha1.Reaper) {
+
 }
--- pkg/backend/backend_test.go
+++ pkg/backend/backend_test.go
@@ -6,6 +6,7 @@ import (
 )
 
 func TestValidate(t *testing.T) {
+	backendService := &defaultBackendService{}
 	tests := []struct{
 		name         string
 		serverConfig v1alpha1.ServerConfig
@@ -40,12 +41,35 @@ func TestValidate(t *testing.T) {
 			},
 			expected: ClusterNameRequired,
 		},
+		{
+			name: "CassandraBackendNoContactPoints",
+			serverConfig: v1alpha1.ServerConfig{
+				StorageType: v1alpha1.Cassandra,
+				CassandraBackend: &v1alpha1.CassandraBackend{
+					ClusterName: "test",
+				},
+			},
+			expected: ContactPointsRequired,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Validate(tt.serverConfig); got != tt.expected {
+			if got := backendService.Validate(tt.serverConfig); got != tt.expected {
 				t.Errorf("expected (%s), got (%s)", tt.expected, got)
 			}
 		})
 	}
 }
+
+//func TestCheckDefaults(t *testing.T) {
+//	backendService := &defaultBackendService{}
+//	tests := []struct{
+//		name         string
+//		serverConfig *v1alpha1.ServerConfig
+//		bool         updated
+//	}{
+//		{
+//			name: "CassandraBackendWithKeyspaceNotSet"
+//		}
+//	}
+//}
