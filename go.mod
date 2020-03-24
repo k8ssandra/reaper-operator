6011db92410330dcb7a89ec87d2a21b114d98fe5
John Sanda <john.sanda@gmail.com>
2020-03-23 23:50:01 -0400

    add unit test for deleting cluster

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/operator-framework/operator-sdk v0.14.0
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/pflag v1.0.5
+	github.com/stretchr/testify v1.5.1
 	golang.org/x/sys v0.0.0-20200120151820-655fe14d7479 // indirect
 	gopkg.in/yaml.v2 v2.2.4
 	k8s.io/api v0.0.0
--- pkg/controller/reaper/reconcile_test.go
+++ pkg/controller/reaper/reconcile_test.go
@@ -2,7 +2,7 @@ package reaper
 
 import (
 	"context"
-	"github.com/bmizerany/assert"
+	"github.com/stretchr/testify/assert"
 	reapergo "github.com/jsanda/reaper-client-go/reaper"
 	"github.com/thelastpickle/reaper-operator/pkg/apis"
 	"github.com/thelastpickle/reaper-operator/pkg/apis/reaper/v1alpha1"
@@ -82,6 +82,7 @@ func TestReconcilers(t *testing.T) {
 	t.Run("DeploymentResourceRequirements", testDeploymentResourceRequirements)
 	t.Run("DeploymentAffinity", testDeploymentAffinity)
 	t.Run("AddCluster", testAddCluster)
+	t.Run("DeleteCluster", testDeleteCluster)
 }
 
 func testReconcileConfigMapNotFound(t *testing.T) {
@@ -602,6 +603,54 @@ func testAddCluster(t *testing.T) {
 		},
 	}
 	assert.Equal(t, expected, reaper.Status.Clusters)
+}
+
+func testDeleteCluster(t *testing.T) {
+	reaper := &v1alpha1.Reaper{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: namespace,
+			Name: name,
+		},
+		Spec: v1alpha1.ReaperSpec{
+		},
+		Status: v1alpha1.ReaperStatus{
+			Clusters: []v1alpha1.CassandraCluster{
+				{
+					Name: "cluster-1",
+					Service: v1alpha1.CassandraService{
+						Name: "cluster-1",
+						Namespace: "default",
+					},
+				},
+			},
+		},
+	}
+
+	objs := []runtime.Object{reaper}
+
+	restClient := newFakeRESTClient()
+	r := createClustersReconciler(objs...)
+	r.newRESTClient = func(reaper *v1alpha1.Reaper) (reapergo.ReaperClient, error) {
+		return restClient, nil
+	}
+
+	result, err := r.ReconcileClusters(context.TODO(), reaper)
+	if result == nil {
+		t.Errorf("expected non-nil result")
+	} else if !result.Requeue {
+		t.Errorf("expected requeue")
+	}
+
+	if err != nil {
+		t.Errorf("expected err (nil), got (%s)", err)
+	}
+
+	reaper = &v1alpha1.Reaper{}
+	if err := r.client.Get(context.TODO(), namespaceName, reaper); err != nil {
+		t.Fatalf("failed to get Reaper after reconciling clusters: %s", err)
+	}
+
+	assert.Empty(t, reaper.Status.Clusters, ".Status.Clusters should be empty")
 }
 
 func createReadyDeployment(reaper *v1alpha1.Reaper) *appsv1.Deployment {
