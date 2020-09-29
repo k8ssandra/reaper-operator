7c1c7621f72571d90d82326acb60d8353a052be5
John Sanda <john.sanda@gmail.com>
2020-09-29 12:06:50 -0400

    create deployment, make operator namespace-scoped, and RBAC for deployments

--- api/v1alpha1/reaper_types.go
+++ api/v1alpha1/reaper_types.go
@@ -28,8 +28,7 @@ type ReaperSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of Reaper. Edit Reaper_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	Image string `json:"image,omitempty"`
 }
 
 // ReaperStatus defines the observed state of Reaper
--- controllers/reaper_controller.go
+++ controllers/reaper_controller.go
@@ -18,13 +18,20 @@ package controllers
 
 import (
 	"context"
+	"time"
 
 	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	reaperv1alpha1 "github.com/thelastpickle/reaper-operator/api/v1alpha1"
+	api "github.com/thelastpickle/reaper-operator/api/v1alpha1"
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // ReaperReconciler reconciles a Reaper object
@@ -34,20 +41,113 @@ type ReaperReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-// +kubebuilder:rbac:groups=reaper.cassandra-reaper.io,resources=reapers,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=reaper.cassandra-reaper.io,resources=reapers/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=reaper.cassandra-reaper.io,namespace="reaper-operator",resources=reapers,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=reaper.cassandra-reaper.io,namespace="reaper-operator",resources=reapers/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups="apps",namespace="reaper-operator",resources=deployments,verbs=get;list;watch;create;update;patch;delete
 
 func (r *ReaperReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
+	ctx := context.Background()
 	_ = r.Log.WithValues("reaper", req.NamespacedName)
 
-	// your logic here
+	// Fetch the Reaper instance
+	instance := &api.Reaper{}
+	err := r.Get(ctx, req.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{Requeue: true, RequeueAfter: 10 * time.Second}, err
+	}
+
+	instance = instance.DeepCopy()
+
+	deployment := &appsv1.Deployment{}
+	err = r.Get(ctx, types.NamespacedName{Namespace: instance.Namespace, Name: instance.Name}, deployment)
+	if err != nil && errors.IsNotFound(err) {
+		deployment = newDeployment(instance)
+		r.Log.Info("creating deployment")
+		if err = r.Create(ctx, deployment); err != nil {
+			r.Log.Error(err, "failed to create deployment")
+			return ctrl.Result{Requeue: true, RequeueAfter: 10 * time.Second}, err
+		} else {
+			return ctrl.Result{}, nil
+		}
+	}
 
 	return ctrl.Result{}, nil
 }
 
+func newDeployment(reaper *api.Reaper) *appsv1.Deployment {
+	selector := metav1.LabelSelector{
+		MatchExpressions: []metav1.LabelSelectorRequirement{
+			{
+				Key:      "app",
+				Operator: metav1.LabelSelectorOpIn,
+				Values:   []string{"reaper"},
+			},
+			{
+				Key:      "reaper",
+				Operator: metav1.LabelSelectorOpIn,
+				Values:   []string{reaper.Name},
+			},
+		},
+	}
+
+	healthProbe := &corev1.Probe{
+		Handler: corev1.Handler{
+			HTTPGet: &corev1.HTTPGetAction{
+				Path: "/healthcheck",
+				Port: intstr.FromInt(8081),
+			},
+		},
+		InitialDelaySeconds: 10,
+		PeriodSeconds:       3,
+	}
+
+	return &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: reaper.Namespace,
+			Name:      reaper.Name,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Selector: &selector,
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app":    "reaper",
+						"reaper": reaper.Name,
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:            "reaper",
+							ImagePullPolicy: corev1.PullIfNotPresent,
+							Image:           "thelastpickle/cassandra-reaper:2.0.5",
+							Ports: []corev1.ContainerPort{
+								{
+									Name:          "ui",
+									ContainerPort: 8080,
+									Protocol:      "TCP",
+								},
+								{
+									Name:          "admin",
+									ContainerPort: 8081,
+									Protocol:      "TCP",
+								},
+							},
+							LivenessProbe:  healthProbe,
+							ReadinessProbe: healthProbe,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
 func (r *ReaperReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&reaperv1alpha1.Reaper{}).
+		For(&api.Reaper{}).
 		Complete(r)
 }
