a4f636f6780cfbe25057dc41845cf02f33997f06
John Sanda <john.sanda@gmail.com>
2020-09-30 16:24:31 -0400

    check for schema job creation

--- controllers/reaper_controller_test.go
+++ controllers/reaper_controller_test.go
@@ -8,14 +8,19 @@ import (
 	. "github.com/onsi/gomega"
 	api "github.com/thelastpickle/reaper-operator/api/v1alpha1"
 	appsv1 "k8s.io/api/apps/v1"
+	v1batch "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 var _ = Describe("Reaper controller", func() {
 	const (
-		ReaperName      = "test-reaper"
-		ReaperNamespace = "default"
+		ReaperName           = "test-reaper"
+		ReaperNamespace      = "default"
+		CassandraClusterName = "test-cluster"
+		CassandraServiceName = "test-cluster-svc"
 
 		timeout  = time.Second * 10
 		interval = time.Millisecond * 250
@@ -28,8 +33,37 @@ var _ = Describe("Reaper controller", func() {
 				Namespace: ReaperNamespace,
 				Name:      ReaperName,
 			},
+			Spec: api.ReaperSpec{
+				ServerConfig: api.ServerConfig{
+					StorageType: api.StorageTypeCassandra,
+					CassandraBackend: &api.CassandraBackend{
+						ClusterName:      CassandraClusterName,
+						CassandraService: CassandraServiceName,
+					},
+				},
+			},
 		}
 		Expect(k8sClient.Create(context.Background(), reaper)).Should(Succeed())
+
+		By("check that the schema job is created")
+		jobKey := types.NamespacedName{Namespace: reaper.Namespace, Name: reaper.Name + "-schema"}
+		job := &v1batch.Job{}
+
+		Eventually(func() bool {
+			err := k8sClient.Get(context.Background(), jobKey, job)
+			if err != nil {
+				return false
+			}
+			return true
+		}, timeout, interval).Should(BeTrue(), "schema job creation check failed")
+
+		// We need to mock the job completion in order for the deployment to get created
+		jobPatch := client.MergeFrom(job.DeepCopy())
+		job.Status.Conditions = append(job.Status.Conditions, v1batch.JobCondition{
+			Type:   v1batch.JobComplete,
+			Status: corev1.ConditionTrue,
+		})
+		Expect(k8sClient.Status().Patch(context.Background(), job, jobPatch))
 
 		By("check that the deployment is created")
 		deploymentKey := types.NamespacedName{Namespace: ReaperNamespace, Name: ReaperName}
@@ -41,6 +75,6 @@ var _ = Describe("Reaper controller", func() {
 				return false
 			}
 			return true
-		}, timeout, interval)
+		}, timeout, interval).Should(BeTrue(), "deployment creation check failed")
 	})
 })
