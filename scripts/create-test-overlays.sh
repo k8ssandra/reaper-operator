1e15ad98eff27034c855d8467e1d90337f1de231
John Sanda <john.sanda@gmail.com>
2020-12-09 15:45:21 -0500

    Upgrade to Go 1.15 (#24)
    * bump go version
    
    This commit also adds some logic to create-test-overlays to avoid munging an
    existing overlay directory
    
    * update GH action to use Go 1.15

--- scripts/create-test-overlays.sh
+++ scripts/create-test-overlays.sh
@@ -27,8 +27,10 @@ else
 fi
 
 mkdir -p test/config/deploy_reaper_test/overlays/forks/$overlay
+kustomization_file=test/config/deploy_reaper_test/overlays/forks/$overlay/kustomization.yaml
 
-cat <<EOT >> test/config/deploy_reaper_test/overlays/forks/$overlay/kustomization.yaml
+if [ ! -f $kustomization_file ]; then
+  cat <<EOT >> test/config/deploy_reaper_test/overlays/forks/$overlay/kustomization.yaml
 resources:
   - ../../../base
 
@@ -37,3 +39,4 @@ images:
     newName: $registry/$org/reaper-operator
     newTag: $tag
 EOT
+fi
